30117e9e0bebef62137935e08d94c2f03193fced
Emmanuel Dania <e.dania@alustudent.com>
2025-12-16 14:10:27 +0100

    server update

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,13 +1,35 @@
 import express from 'express';
-import { listProduct, addProduct, removeProduct, singleProduct } from '../controllers/productController.js'
+import {
+  listProduct,
+  addProduct,
+  removeProduct,
+  singleProduct
+} from '../controllers/productController.js'
 import upload from '../middleware/multer.js';
 import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
-productRouter.post('/remove',adminAuth,removeProduct);
-productRouter.post('/single',singleProduct);
-productRouter.get('/list',listProduct);
+// ✅ Allow preflight requests
+productRouter.options("*", (req, res) => {
+  res.sendStatus(200);
+});
 
-export default productRouter
+// Routes
+productRouter.post(
+  '/add',
+  adminAuth,
+  upload.fields([
+    { name: 'image1', maxCount: 1 },
+    { name: 'image2', maxCount: 1 },
+    { name: 'image3', maxCount: 1 },
+    { name: 'image4', maxCount: 1 }
+  ]),
+  addProduct
+);
+
+productRouter.post('/remove', adminAuth, removeProduct);
+productRouter.post('/single', singleProduct);
+productRouter.get('/list', listProduct);
+
+export default productRouter;
